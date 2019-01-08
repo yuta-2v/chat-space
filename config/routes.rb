c3525a2560103a4b07f564f1507078b44a87c679
yuta-2v <y@yn.local>
2019-01-08 16:19:43 +0900

    Add edit function

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
   get 'messages' => 'messages#index'
+  resources :users, only: [:edit, :update]
 end
