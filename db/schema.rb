498eebb7f80813a27644a812fa05203ca1208d60
yuta-2v <y@yn.local>
2019-01-10 13:22:50 +0900

    Generate message model
    migrate
    Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108075803) do
+ActiveRecord::Schema.define(version: 20190110040947) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20190108075803) do
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_members_on_group_id", using: :btree
     t.index ["user_id"], name: "index_members_on_user_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "message",    limit: 65535, null: false
+    t.text     "image",      limit: 65535
+    t.integer  "user_id",                  null: false
+    t.integer  "group_id",                 null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
