4f0832773dfec764640b4dc791e02895bdcbbad5
yuta-2v <yut.a.rumin@gmail.com>
2019-01-30 09:48:57 +0900

    Convert var

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,13 +1,12 @@
 $(function(){
+  var $search_result = $("#user-search-result");
+
   function appendUsers(user){
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">
-                    ${user.name}
-                  </p>
+                  <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                 </div>`;
-    $('#user-search-result').append(html);
-    console.log("append");
+    $search_result.append(html);
   }
 
   function appendUser(user){
@@ -15,11 +14,11 @@ $(function(){
                   ${user}
                 </div>
     `;
-    $('#user-search-result').append(html);
+    $search_result.append(html);
   }
 
   $('#user-search-field').keyup(function(){
-    var input =$("#user-search-field").val();
+    var input = $.trim($("#user-search-field").val());
     $.ajax({
       url: '/users',
       type: 'GET',
@@ -27,7 +26,7 @@ $(function(){
       dataType: 'json'
     })
     .done(function(users){
-      $('#user-search-result').empty();
+      $search_result.empty();
       if (users.length !== 0){
         users.forEach(function(user){
           appendUsers(user);
