4282590efe5d5031ac30154002763755d05c4c0e
yuta-2v <yut.a.rumin@gmail.com>
2019-01-30 09:40:55 +0900

    ajax Func keyup done

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,4 +1,27 @@
 $(function(){
+  function appendUsers(user){
+  }
+
+  function appendUser(user){
+  }
+
   $('#user-search-field').keyup(function(){
+    var input =$("#user-search-field").val();
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUsers(user);
+        });
+      } else {
+        appendUser('nobody');
+      }
+    })
   })
 });
