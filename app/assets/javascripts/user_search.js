97d0f0c0bd07681f794c76dae6641546712d5aea
yuta-2v <yut.a.rumin@gmail.com>
2019-01-30 09:52:45 +0900

    Add func/view add_user

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -17,6 +17,16 @@ $(function(){
     $search_result.append(html);
   }
 
+  function addUser(id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>
+    `;
+    $search_result.append(html);
+  }
+
   $('#user-search-field').keyup(function(){
     var input = $.trim($("#user-search-field").val());
     $.ajax({
@@ -40,6 +50,10 @@ $(function(){
     })
   })
 
-    $("#user-search-result").on('click', ".chat-group-user__btn--add", function(){
+  $($search_result).on('click', ".chat-group-user__btn--add", function(){
+    var id = $(this).attr("data-user-id")
+    var name = $(this).attr("data-user-name")
+    var html = addUser(id,name)
+    $(this).parent().remove();
   });
 });
