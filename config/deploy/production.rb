12e137a3fa8d589eab5156b216790c126007b5e2
yuta-2v <yut.a.rumin@gmail.com>
2019-02-01 19:46:57 +0900

    basic confirm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '18.182.91.246', user: 'ec2-user', roles: %w{app db web}
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
